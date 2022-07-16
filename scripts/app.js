64d0a0760e117b8c349e6cec8a811992151ae1e8
dr-veronica <veronica.deronzi@gmail.com>
2022-07-16 11:25:00 +0100

    draft2

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,13 @@ function init () {
     const cellCount = width * width
     const cells = []
 
+    const charPlayer = 'player'
+    const startingPosition = 99
+
+    const charEnemy = 'enemy'
+    const startPosition = 0
+
+
 
     function createGrid(){
         for(let i = 0; i < cellCount; i++){
@@ -15,7 +22,13 @@ function init () {
           cells.push(cell)
           grid.appendChild(cell)
         }
+        add(startingPosition, charPlayer)
+        add(startPosition, charEnemy)
       }
+    function add(position, character){
+        cells[position].classList.add(character)
+    }
+
  createGrid()
 }
 window.addEventListener('DOMContentLoaded', init)
